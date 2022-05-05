e09e77b26499c37b0f5aaff782743df72345b448
Zaid Mohamad <zaid_s72@yahoo.com>
2022-05-04 23:55:01 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Course-zaid"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-action"
     }
   }
 }
